e8682cf3441d3fa1fc55ee584b8fc1ee791d8438
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-05-14 10:13:17 +1000

    seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+books = [{
+    title: 'book_1', 
+    description: 'happy',
+    isbn: "0320329"
+},
+{
+    title: 'book_2',
+    description: 'love',
+    isbn: '2342482'
+}
+]
+
+Book.create!(books) {|b| p b.title}
